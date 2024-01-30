955a04a6799156278748cddd830ea9a9cf42464c
Lotfi <lotfi.m.bendiaf@gmail.com>
2024-01-30 16:00:22 +0100

    Hero image updated

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex shrink-0 items-end rounded-lg bg-sky-50 p-4">
-        <SynergyLogo size={150}/>
+        <SynergyLogo size={200}/>
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
@@ -31,14 +31,14 @@ export default function Page() {
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
         <Image
-          src="/hero-desktop.png"
-          width={1000}
-          height={760}
+          src="/hero-desktop1.png"
+          width={1200}
+          height={860}
           className="hidden md:block"
           alt="Screenshots of the dashboard project showing desktop version"
         />
         <Image
-          src="/hero-mobile.png"
+          src="/hero-mobile1.png"
           width={560}
           height={620}
           className="block md:hidden"
